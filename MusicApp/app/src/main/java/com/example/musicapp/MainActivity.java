b2863b2e6372a01c4e51ff4ebe1538074e44ad31
Nguyen Cong Chinh <94957089+NguyenCongCh@users.noreply.github.com>
2023-03-17 22:12:53 +0700

    a
    them activity

--- MusicApp/app/src/main/java/com/example/musicapp/MainActivity.java
+++ MusicApp/app/src/main/java/com/example/musicapp/MainActivity.java
@@ -1,73 +1,45 @@
-package com.example.musicapp;
+package com.liem.musicapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
-import android.content.Intent;
-import android.media.MediaPlayer;
-import android.net.Uri;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.SeekBar;
-import android.widget.TextView;
 
-import java.io.File;
-import java.util.ArrayList;
+import com.google.android.material.tabs.TabLayout;
+import com.liem.musicapp.R;
+import com.liem.musicapp.adapters.ViewPagerAdapterMain;
+import com.liem.musicapp.fragments.TimKiemFragment;
+import com.liem.musicapp.fragments.TranngChuFragment;
 
 public class MainActivity extends AppCompatActivity {
-    Button btnplay,btnnext,btnprevious,btnff,btnfr;
-    TextView tvsongname,tvstart,tvstop;
-    SeekBar seekmusic;
 
-    String song_name;
-    public static final String EXTRA_NAME="song_name";
-    static MediaPlayer mediaPlayer;
-    int position;
-    ArrayList<File> mysongs;
+    TabLayout tabLayout;
+    ViewPager viewPager;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnprevious=findViewById(R.id.btnprevious);
-        btnnext=findViewById(R.id.btnnext);
-        btnplay=findViewById(R.id.playbtn);
-        btnff=findViewById(R.id.btnff);
-        btnfr=findViewById(R.id.btnfr);
-        tvsongname=findViewById(R.id.tvsongname);
-        tvstop=findViewById(R.id.tvstop);
-        seekmusic=findViewById(R.id.seekbar);
 
-        if(mediaPlayer !=null){
-            mediaPlayer.stop();
-            mediaPlayer.release();
-        }
-        Intent i=getIntent();
-        Bundle bundle=i.getExtras();
-
-        mysongs=(ArrayList)bundle.getParcelableArrayList("songs"); // key song get on the songlist
-        String songname=i.getStringExtra("songname"); // songname get on the songlist
-        position=bundle.getInt("pos",0);
-        tvsongname.setSelected(true);
-        Uri uri=Uri.parse(mysongs.get(position).toString());
-        song_name=mysongs.get(position).getName();
-        tvsongname.setText(song_name);
-
-        mediaPlayer=MediaPlayer.create(getApplicationContext(),uri);
-        mediaPlayer.start();
-
-        btnplay.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(mediaPlayer.isPlaying()){
-                    btnplay.setBackgroundResource(R.drawable.ic_play);
-                    mediaPlayer.pause();
-                }
-                else {
-                    btnplay.setBackgroundResource(R.drawable.ic_pause);
-                    mediaPlayer.start();
-                }
-            }
-        });
+        mapping();
+        init();
     }
 
+    public void mapping() {
+        tabLayout = (TabLayout) findViewById(R.id.MainActivity_TabLayout);
+        viewPager = (ViewPager) findViewById(R.id.MainActivity_ViewPager);
+    }
+
+    public void init() {
+        ViewPagerAdapterMain mainViewPagerAdapter = new ViewPagerAdapterMain(getSupportFragmentManager());
+        mainViewPagerAdapter.addFragment(new TranngChuFragment(), "Trang chủ");
+        mainViewPagerAdapter.addFragment(new TimKiemFragment(), "Tìm kiếm");
+        viewPager.setAdapter(mainViewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.getTabAt(0).setIcon(R.drawable.icontrangchu);
+        tabLayout.getTabAt(1).setIcon(R.drawable.icontimkiem);
+        tabLayout.setBackgroundColor(Color.rgb(177, 177, 177));
+    }
 }
