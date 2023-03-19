3c996c68aa343c872001f945a7c11cab344c7d3b
Ds258 <ds25082002@gmail.com>
2023-03-19 20:47:38 +0700

    Fix TrangChu 1

--- MusicApp/app/src/main/java/com/example/musicapp/TranngChuFragment.java
+++ MusicApp/app/src/main/java/com/example/musicapp/TranngChuFragment.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.musicapp.R;
 import com.liem.musicapp.R;
 
 public class TranngChuFragment extends Fragment {
