da81f046503f7222602039576745bc39999fe0a9
XXMISSILEXX <89600178+XXMISSILEXX@users.noreply.github.com>
2023-03-23 10:50:54 +0700

    Them ImageView vao Adapter

--- MusicApp/app/src/main/java/com/example/musicapp/RecyclerAdapterChuDe.java
+++ MusicApp/app/src/main/java/com/example/musicapp/RecyclerAdapterChuDe.java
@@ -51,7 +51,7 @@ public class RecyclerAdapterChuDe extends RecyclerView.Adapter<RecyclerAdapterCh
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         TextView txtChude;
-        ImageView imgsrc;
+        ImageView img;
 
         public ViewHolder(@NonNull @NotNull View itemView) {
             super(itemView);
