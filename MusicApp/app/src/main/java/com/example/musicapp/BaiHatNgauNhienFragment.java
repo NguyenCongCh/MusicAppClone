59beb95772678bd6b28cf1c5511f0e01c6b8b690
XXMISSILEXX <89600178+XXMISSILEXX@users.noreply.github.com>
2023-03-23 10:47:54 +0700

    Update BaiHatNgauNhienFragment.java

--- MusicApp/app/src/main/java/com/example/musicapp/BaiHatNgauNhienFragment.java
+++ MusicApp/app/src/main/java/com/example/musicapp/BaiHatNgauNhienFragment.java
@@ -37,7 +37,7 @@ public class BaiHatNgauNhienFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        // Inflate the layout for this fragment (lay view ra ngoai)
         view = inflater.inflate(R.layout.fragment_bai_hat_ngau_nhien, container, false);
         recyclerView = view.findViewById(R.id.fmBaiHatNgauNhien_recyclerView);
         getData();
